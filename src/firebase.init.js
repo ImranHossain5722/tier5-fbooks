30c8e1f698c7644a2f994654c264300dff2411f9
ImranHossain5722 <imd572258@gmail.com>
2022-08-15 07:00:17 +0600

    add some screenshot

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaafPbJ9QM42AiWCgnjCRGcPbCPF0oSmM",
+  authDomain: "facebookclone-90026.firebaseapp.com",
+  projectId: "facebookclone-90026",
+  storageBucket: "facebookclone-90026.appspot.com",
+  messagingSenderId: "1011310313210",
+  appId: "1:1011310313210:web:df52f6d184ee0229f29343"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
